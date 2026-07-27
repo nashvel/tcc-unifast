434256cb91c3f3f3c299e5eb773f4d96649eb8a9
nashvel <nash.backup21@gmail.com>
2026-07-27 19:31:55 +0800

    chore(database): update academic programs seeder to exact requirements

--- backend/database/migrations/2026_07_26_120000_seed_tcc_academic_programs.php
+++ backend/database/migrations/2026_07_26_120000_seed_tcc_academic_programs.php
@@ -8,17 +8,12 @@ return new class extends Migration {
     {
         $now = now();
         $programs = [
-            ['BSIT', 'Bachelor of Science in Information Technology'],
-            ['BSBA', 'Bachelor of Science in Business Administration'],
-            ['BEED', 'Bachelor in Elementary Education'],
-            ['BSED', 'Bachelor of Secondary Education'],
-            ['BSPA', 'Bachelor of Science in Public Administration'],
-            ['BSCRIM', 'Bachelor of Science in Criminology'],
             ['ABSocio', 'Bachelor of Arts in Sociology'],
-            ['DEVCOM', 'Bachelor of Science in Development Communication'],
-            ['BSFT', 'Bachelor of Science in Food Technology'],
-            ['BSAT', 'Bachelor of Science in Automotive Technology'],
-            ['BSET', 'Bachelor of Science in Electronics Technology'],
+            ['BEED', 'Bachelor in Elementary Education'],
+            ['BSBA', 'Bachelor of Science in Business Administration'],
+            ['BSCRIM', 'Bachelor of Science in Criminology'],
+            ['BSED', 'Bachelor of Secondary Education'],
+            ['BSIT', 'Bachelor of Science in Information Technology'],
         ];
 
         foreach ($programs as [$code, $name]) {
@@ -47,7 +42,7 @@ return new class extends Migration {
     public function down(): void
     {
         DB::table('academic_programs')->whereIn('code', [
-            'BSBA', 'BEED', 'BSED', 'BSPA', 'BSCRIM', 'ABSocio', 'DEVCOM', 'BSFT', 'BSAT', 'BSET',
+            'ABSocio', 'BEED', 'BSBA', 'BSCRIM', 'BSED', 'BSIT',
         ])->delete();
     }
 };
